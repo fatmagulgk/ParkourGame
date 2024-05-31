d5db81af4ce1918df20999cd4301606921b77a07
Fatma Gül Gök <148465338+fatmagulgk@users.noreply.github.com>
2024-05-31 15:16:41 +0300

    Add Player

---
+++ Assets/_Developer_FatmaGul/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 3;
+    public float JumpPower = 2;
+
+
+}
