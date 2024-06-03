0a5e62ed407731f1134eb4e4bfeb336e28745cda
Fatma Gül Gök <148465338+fatmagulgk@users.noreply.github.com>
2024-06-03 15:23:27 +0300

    Add Player Input System

--- Assets/_Developer_FatmaGul/Scripts/Player/PlayerMotor.cs
+++ Assets/_Developer_FatmaGul/Scripts/Player/PlayerMotor.cs
@@ -5,8 +5,34 @@ using UnityEngine;
 public class PlayerMotor : MonoBehaviour
 {
     PlayerMovement PlayerMovement;
+    Rigidbody rb;
     private void Awake()
     {
+        PlayerMovement = GetComponent<PlayerMovement>();
+        rb = GetComponent<Rigidbody>();
+
+    }
+    
+    private void FixedUpdate()
+    {
+        Vector3 movePos = transform.position;
+        if (Input.GetKey(KeyCode.W))//GetKey basili tuttugun surece calisir.Ancak GetKeyDown bastigimiz anda calisir basili tutmaya devam edersek calismaz.Ve birakip tekrar basmamiz gerekir.
+        {
+           movePos+= Vector3.forward*(Time.deltaTime*PlayerMovement.Speed);//Rigidbody'e kendi pozisyonumuzun ileri(forward) yonunde hizimiza gore ve rotasyonumuza gore hareket etmesini saglar. 
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            movePos +=  Vector3.back * (Time.deltaTime * PlayerMovement.Speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movePos += Vector3.left* (Time.deltaTime * PlayerMovement.Speed);      
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movePos += Vector3.right * (Time.deltaTime * PlayerMovement.Speed);
+        }
         
+        rb.MovePosition(movePos);
     }
 }
--- Assets/_Developer_FatmaGul/Scripts/Player/PlayerMovement.cs
+++ Assets/_Developer_FatmaGul/Scripts/Player/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerMotor))]//Ben player movement olarak player motorsuz yasayamam demek.Eðer bir yere player movement eklendiyse player motorda eklenir.
 public class PlayerMovement : MonoBehaviour
 {
     public float Speed = 3;
